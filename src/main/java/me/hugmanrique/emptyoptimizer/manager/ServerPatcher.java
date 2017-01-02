a0e868e804d79989d421dca49fdc18a8ecba608b
hugmanrique <hugmanrique@gmail.com>
2017-01-02 17:22:52 -0500

    Plugin is broken

--- src/main/java/me/hugmanrique/emptyoptimizer/manager/ServerPatcher.java
+++ src/main/java/me/hugmanrique/emptyoptimizer/manager/ServerPatcher.java
@@ -1,17 +1,12 @@
 package me.hugmanrique.emptyoptimizer.manager;
 
 import me.hugmanrique.emptyoptimizer.Main;
-import org.bukkit.Bukkit;
 import org.objectweb.asm.ClassReader;
-import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.*;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
-import java.security.CodeSource;
 import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Level;
@@ -19,49 +14,29 @@ import java.util.logging.Logger;
 
 /**
  * @author Hugmanrique
- * @since 01/01/2017
+ * @since 02/01/2017
  */
-public class TickChanger {
+public class ServerPatcher {
     private Main main;
 
-    public TickChanger(Main main) {
+    public ServerPatcher(Main main) {
         this.main = main;
-        modifyBytecode();
     }
 
-    private Class<?> getNMSClass() throws ClassNotFoundException {
-        String name = main.getServer().getClass().getPackage().getName();
-        String version = name.substring(name.lastIndexOf('.') + 1);
+    public void patchSpigot() {
+        getLogger().log(Level.INFO, "Patching MinecraftServer class");
 
-        return Class.forName("net.minecraft.server." + version + ".MinecraftServer");
-    }
+        InputStream byteCode = getServerBytecode();
 
-    private InputStream getServerBytecode() {
-        try {
-            Class<?> clazz = getNMSClass();
-            String className = clazz.getCanonicalName().replaceAll("\\.", "/") + ".class";
-
-            return clazz.getClassLoader().getResourceAsStream(className);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    private void modifyBytecode() {
-        getLogger().log(Level.INFO, "Applying ASM to Minecraft run method");
-
-        InputStream in = getServerBytecode();
-
-        if (in == null) {
-            getLogger().log(Level.SEVERE, "Couldn't find MinecraftServer bytecode, are you using a custom Minecraft server build?");
+        if (byteCode == null) {
+            getLogger().log(Level.SEVERE, "Couldn't find MinecraftServer class");
             return;
         }
 
         ClassNode classNode = new ClassNode();
 
         try {
-            ClassReader reader = new ClassReader(in);
+            ClassReader reader = new ClassReader(byteCode);
             reader.accept(classNode, 0);
 
             Iterator<MethodNode> methods = classNode.methods.iterator();
@@ -112,15 +87,36 @@ public class TickChanger {
                 method.instructions.add(list);
             }
 
-            writeNewBytecode(classNode);
+            writeToJar(classNode);
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        getLogger().log(Level.INFO, "Patched successfully. Restarting...");
+        main.getServer().shutdown();
     }
 
-    private void writeNewBytecode(ClassNode node) {
-        ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_MAXS | ClassWriter.COMPUTE_FRAMES);
-        node.accept(writer);
+    private void writeToJar(ClassNode node) {
+
+    }
+
+    private InputStream getServerBytecode() {
+        try {
+            Class<?> clazz = getNMSClass();
+            String className = clazz.getCanonicalName().replaceAll("\\.", "/") + ".class";
+
+            return clazz.getClassLoader().getResourceAsStream(className);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private Class<?> getNMSClass() throws ClassNotFoundException {
+        String name = main.getServer().getClass().getPackage().getName();
+        String version = name.substring(name.lastIndexOf('.') + 1);
+
+        return Class.forName("net.minecraft.server." + version + ".MinecraftServer");
     }
 
     private Logger getLogger() {
